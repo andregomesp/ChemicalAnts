47cc48a72f96063d8d461e06ebb6dc3364955856
Z- Usuario Temporario de Banco de Dados <fa7bd@7setlabs.local>
2019-05-21 21:24:44 -0300

    creditos finales

--- src/engine/animations.lua
+++ src/engine/animations.lua
@@ -1,5 +1,5 @@
 local M = {}
-
+local background = nil
 function M:drawTouchInfo(group, timers)
     local tapToAdvanceText = display.newText({parent=group, text="Tap the screen to advance",
         x = display.viewableContentWidth / 5 , y = display.viewableContentHeight - 40,
@@ -15,6 +15,31 @@ function M:drawTouchInfo(group, timers)
         table.insert(timers, fadeOutTimer)
         table.insert(timers, fadeInTimer)
     return tapToAdvanceText
+end
+
+local function handleBackground(event)
+    local needToMoveGroup = background:checkBackgroundNeedsRebuild()
+    if needToMoveGroup ~= false then
+        background:moveBackgroundGroupIntro(needToMoveGroup)
+    end
+    return true
+end
+
+local function initiateRuntimeBkground()
+    Runtime:addEventListener( "enterFrame", handleBackground)
+end
+
+function M:drawBackgroundScene(backgroundGroup, leftBackgroundGroup, rightBackgroundGroup)
+    local backgroundImage = display.newRect(backgroundGroup, display.contentCenterX,
+    display.contentCenterY, display.viewableContentWidth, display.viewableContentHeight)
+    backgroundImage:setFillColor(0.435, 0.623, 0.196)
+    local backgroundFactory = require("src.domain.background")
+    background = backgroundFactory:new(nil, backgroundImage, leftBackgroundGroup,
+        rightBackgroundGroup, backgroundGroup)
+    background:buildBackground(130, {leftWall="assets/images/commons/scenario/trees.png",
+        rightWall="assets/images/commons/scenario/trees.png", stageNumber=0})
+    initiateRuntimeBkground()
+
 end
 
 return M
--- src/scenes/gameover.lua
+++ src/scenes/gameover.lua
@@ -1,13 +1,17 @@
 local composer = require ("composer")
 local scene = composer.newScene()
+local backgroundGroup = display.newGroup()
+local leftBackgroundGroup = display.newGroup()
+local rightBackgroundGroup = display.newGroup()
 local backScreenGroup = display.newGroup()
 local textGroup = display.newGroup()
+local sceneGroup = nil
 local isShowing = false
 local lines = {}
 
 local function drawGameOverScene()
-    local congrats = display.newText({parent = textGroup, text="Congratulations!", 
-    x=display.contentCenterX, y=display.contentCenterY})
+    -- local congrats = display.newText({parent = textGroup, text="Congratulations!",
+    -- x=display.contentCenterX, y=display.contentCenterY})
 end
 
 local function drawCredits()
@@ -15,14 +19,30 @@ local function drawCredits()
     local credits = creditsFactory:getCredits()
     local counter = 0
     for k, v in ipairs(credits) do
-        local line = display.newText({parent=textGroup, text=v, 
-            font="ELDERWEISS-Regular", x=display.contentCenterX, 
-            y=-100 + (20 * counter), fontSize = 25})
+        local line = display.newText({parent=textGroup, text=v,
+            font="ELDERWEISS-Regular", x=display.contentCenterX,
+            y=display.viewableContentWidth * 2 + (50 * counter), fontSize = 20,
+            height = 0, align="center"})
+            line.anchorX = 0.5
+        line:setFillColor(0.13, 0, 0.1)
+        table.insert(lines, line)
+        counter = counter + 1
     end
-    table:insert(lines, line)
+end
+
+local function animateCredits()
+    for k, v in ipairs(lines) do
+        transition.to(v, {y=v.y - display.viewableContentWidth * 6.2, time=24200})
+    end
+end
+
+local function drawBackground()
+    local background = require("src.engine.animations")
+    background:drawBackgroundScene(backgroundGroup, leftBackgroundGroup, rightBackgroundGroup)
 end
 
 function scene:create( event )
+    sceneGroup = self.view -- add display objects to this group
 end
 
 function scene:show(event)
@@ -30,6 +50,8 @@ function scene:show(event)
         isShowing = true
         drawGameOverScene()
         drawCredits()
+        animateCredits()
+        drawBackground()
     end
 end
 function scene:hide(event)
